88408dd512bdfa333986c1d319f45e8f3c383d9a
dennyraymondd <denny.raymond@cashenable.com>
2022-03-16 00:53:39 +0700

    Created my first API :D

---
+++ main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI, HTTPException
+from models import User, Gender, Role, UserUpdateRequest
+from typing import List
+from uuid import UUID, uuid4
+
+app = FastAPI()
+
+
+# db
+db: List[User] = [
+    User(id=uuid4(), first_name="Denny", last_name="Raymond",
+         gender=Gender.male, roles=[Role.admin, Role.user],),
+    User(id=uuid4(), first_name="Aurellia", last_name="Tanica",
+         gender=Gender.female, roles=[Role.student])
+]
+
+
+@app.get("/")
+async def root():
+    print('root calledeeeeeeeeeeeeee')
+    return {"Hello": "World"}
+
+
+@app.get("/api/v1/users")
+async def fetch_all_users():
+    return db
+
+
+@app.post("/api/v1/users")
+async def create_new_user(user: User):
+    db.append(user)
+    return {"id": user.id}
+
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return
+
+    raise HTTPException(
+        status_code=404, detail=f"user with id {user_id} not exists")
+
+
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_id: UUID, user_update: UserUpdateRequest):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.gender is not None:
+                user.gender = user_update.gender
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return
+
+    raise HTTPException(
+        status_code=404, detail=f"user with id: {user_id} not exists")
